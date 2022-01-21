6bf669b0aacdf881bbe02dc06dd96e44ea7eb1dd
didier lemaitre <did.lemaitre@free.fr>
2022-01-21 12:56:52 +0100

    correction indentations

--- save-wordpress.sh
+++ save-wordpress.sh
@@ -7,16 +7,16 @@ ftppass="pass"
 ftpdir="/backups/"
 wppath="/var/www/"
 wpfolder="domain.com"
-nullfolder="/tmp/null"
+retime="5"
 
 # supprimer les vieilles sauvegarde de plus de 5 jours
-find $ftpdir* -type d -mtime +2 -exec rm -rdf {} \;
+find $ftpdir* -type d -mtime +$retime -exec rm -rdf {} \;
 
 # Si déjà sauvegardé aujourd'hui on sort
-if [ -d "$ftpdir"wordpress-$(date +"%d-%m-%Y")"" ]; then
+if [ -d "$ftpdir"wordpress-$(date +"%d-%m-%Y")"" ];
+then
   echo "bye"
-  else
-
+else
   # creation  du dossier local recevant les backups
   mkdir -v $ftpdir"wordpress-$(date +"%d-%m-%Y")"
   # creation  du dossier recevant les dump de la base mysql dans l'arborescence du site Wordpress
@@ -26,12 +26,12 @@ if [ -d "$ftpdir"wordpress-$(date +"%d-%m-%Y")"" ]; then
   # Tar compressé de l'arborescence du site Wordpress qui contient aussi le Dump SQL
   cd $wppath
   tar -v -cpPzf $ftpdir"wordpress-$(date +"%d-%m-%Y")"/WordpressBackup.$(date +"%Y-%m-%d").tar.gz $wpfolder/
-# transfert en FTP du fichier tar vers le serveur FTP
-lftp -u $ftpuser,$ftppass $ftpsite << EOF
-cd $ftpdir
-put $ftpdir"wordpress-$(date +"%d-%m-%Y")"/WordpressBackup.$(date +"%Y-%m-%d").tar.gz
-EOF
-
+  # transfert en FTP du fichier tar.gz vers le serveur FTP
+  lftp -u $ftpuser,$ftppass $ftpsite <<-EOF
+  cd $ftpdir
+  put $ftpdir"wordpress-$(date +"%d-%m-%Y")"/WordpressBackup.$(date +"%Y-%m-%d").tar.gz
+	EOF
+  # suppression de la sauvegarde locale dump en double de la base SQL
+  rm -rf  $wppath$wpfolder$ftpdir"wordpress-$(date +"%d-%m-%Y")"
 fi
-# clear
 echo "fin du script"
